2649f35cbbb207bd24894114ae58cfc3e363970b
Keegan White <keegan28white@gmail.com>
2023-05-04 12:48:43 -0500

    made some changes

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,23 +6,27 @@
         {
             //Number guessing game
             var r = new Random();
-            var favNumber = r.Next(0, 1001); 
+            var favNumber = r.Next(0, 1001);
+            int userInput;
 
-            Console.WriteLine("Pick a number from 1 - 1000");
-            var userInput = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Pick a number from 1 - 1000");
+                userInput = int.Parse(Console.ReadLine());
 
-            if (userInput < favNumber)
-            {
-                Console.WriteLine($"{userInput} is too low");
-            }
-            else if(userInput > favNumber)
-            {
-                Console.WriteLine($"{userInput} is too high");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (userInput != favNumber);
         }
     }
 }
